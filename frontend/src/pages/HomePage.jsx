000e1e11188975ff404194aeee110122b128c0f1
Nouamane Elmkadem <nouamanlamkadmxd@gmail.com>
2023-03-17 11:15:51 +0100

    .

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import logoImg from '../images/logo.png'
 import SimpleBank from '../images/SimpleBank.png'
 import './style.css'
 
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import StarIcon from '@mui/icons-material/StarBorder';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
@@ -31,7 +24,7 @@ function HomePage() {
       >
         <Toolbar sx={{ flexWrap: 'wrap' }}>
           <Typography color="inherit" sx={{ flexGrow: 1, mx: 3 }}>
-            <img src={logoImg} className='logoIdentify' />
+            <img src={logoImg} className='homePageLogo' />
           </Typography>
           <Button href="login" variant="outlined" sx={{ mx: 2 }}>
             Login
@@ -56,7 +49,7 @@ function HomePage() {
             <Button href="register" variant="contained" sx={{ mt: 1.5, width: '340px' }} >
               Get Started
             </Button>
-            <p class="Paragraph">Already a customer? <a href="login" className='Link'>Login here</a>.</p>
+            <p className="Paragraph">Already a customer? <a href="login" className='Link'>Login here</a>.</p>
           </div>
           <div className='divRight'>
             <img src={SimpleBank} className='SimpleBank' />
--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
 
   useEffect(() => {
     document.title = "Login | Simple"
-  },[])
+  }, [])
 
   const handleSubmit = async (event) => {
     event.preventDefault()
@@ -50,75 +50,77 @@ export default function Login() {
   }
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <img src={SimpleImg} className="logoIdentify"/>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <LoadingButton
-            loading={IsLoading}
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </LoadingButton>
-          <Grid container>
-            <Grid item xs>
-              <Link href="forgetpassword" variant="body2">
-                Forgot password?
-              </Link>
+    <div style={{ }}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <img src={SimpleImg} className="logoIdentify" />
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <LoadingButton
+              loading={IsLoading}
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </LoadingButton>
+            <Grid container>
+              <Grid item xs>
+                <Link href="forgetpassword" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="register" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
 
-      <Snackbar
-        open={ShowAlert}
-        autoHideDuration={4000}
-        onClose={() => { setShowAlert(false) }}
-        message={AlertMessage}
-      />
-    </Container>
-  );
+        <Snackbar
+          open={ShowAlert}
+          autoHideDuration={4000}
+          onClose={() => { setShowAlert(false) }}
+          message={AlertMessage}
+        />
+      </Container>
+    </div>
+  )
 }
